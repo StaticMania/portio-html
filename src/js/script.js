146b6141d5b601b3d0fde5dc7b3aa17124084a68
fahim100 <fahimahmmed100@gmail.com>
2020-03-23 17:24:13 +0600

    Added Preloder.

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,63 @@
+// Preloader
+
+function PageLoad() {
+  $("body").removeClass("hidden");
+  TweenMax.to($(".preloader-text"), 1, {
+    force3D: true,
+    opacity: 1,
+    y: 0,
+    delay: 0.2,
+    ease: Power3.easeOut
+  });
+
+  var width = 100,
+    perfData = window.performance.timing, 
+    EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
+    time = parseInt((EstimatedTime / 500) % 50) * 70;
+
+  // Percentage Increment Animation
+  var PercentageID = $("#precent"),
+    start = 01,
+    end = 99,
+    durataion = time;
+  animateValue(PercentageID, start, end, durataion);
+
+  function animateValue(id, start, end, duration) {
+    var range = end - start,
+      current = start,
+      increment = end > start ? 1 : -1,
+      stepTime = Math.abs(Math.floor(duration / range)),
+      obj = $(id);
+
+    var timer = setInterval(function() {
+      current += increment;
+      $(obj).text(current);
+      if (current === end) {
+        clearInterval(timer);
+      }
+    }, stepTime);
+  }
+  // Fading Out Loadbar on Finised
+  setTimeout(function() {
+    TweenMax.to($(".percentage, .inner"), 0.7, {
+      force3D: true,
+      opacity: 0,
+      yPercent: -101,
+      ease: Power3.easeInOut
+    });
+    TweenMax.to($(".preloader-wrap"), 0.7, {
+      force3D: true,
+      yPercent: -150,
+      delay: 0.2,
+      ease: Power3.easeInOut
+    });
+    
+  }, time);
+}
 $(document).ready(function() {
+
+  // preloder
+  PageLoad();
 
   // change-navigation-color
 	$(window).scroll(function () {
@@ -66,7 +125,7 @@ $(document).ready(function() {
     });
     $('.skill__progress_item').addClass('js-animation');
     this.destroy();
-  },{ offset: '50%' });
+  },{ offset: '80%' });
   
   // Testimonial slider
   $('.testimonial__slider').slick({
